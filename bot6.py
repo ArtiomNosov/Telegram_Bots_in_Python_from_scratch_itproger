6775cd902184bb4a2454bfb3348af7d26ec162fa
Artiom <@H20CH3PO2>
2023-06-22 22:34:01 +0300

    Commit 25

--- bot6.py
+++ bot6.py
@@ -7,7 +7,7 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    markup = types.InlineKeyboardMarkup()
+    markup = types.ReplyKeyboardMarkup()
     markup.add(types.InlineKeyboardButton('Открыть веб страницу', web_app=WebAppInfo(url='https://h20ch3po2.github.io/Telegram_Bots_in_Python_from_scratch_itproger/')))
     await message.answer('Привет, мой друг!', reply_markup=markup)
 
