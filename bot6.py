f966761612395cf76591979ffae8d99530fa1743
Artiom <@H20CH3PO2>
2023-06-22 22:39:39 +0300

    Commit 26

--- bot6.py
+++ bot6.py
@@ -11,5 +11,8 @@ async def start(message: types.Message):
     markup.add(types.InlineKeyboardButton('Открыть веб страницу', web_app=WebAppInfo(url='https://h20ch3po2.github.io/Telegram_Bots_in_Python_from_scratch_itproger/')))
     await message.answer('Привет, мой друг!', reply_markup=markup)
 
+@dp.message_handler(content_types=['web_app_data'])
+async def web_app(message: types.Message):
+    await message.answer(message.web_app_data.data)
 
 executor.start_polling(dp)
